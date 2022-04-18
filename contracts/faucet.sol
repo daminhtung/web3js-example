0b6c236e8fb72b526da41b39fa7e75e6032b11f3
daminhtung <daminhtung@gmail.com>
2022-04-18 22:43:52 +0700

    first contract, truffle compile, truffle migrate, truffle console

---
+++ contracts/faucet.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Faucet {
+    uint public unasigned_integer = 10029;
+    int public integer_number = -1222;
+    address private minter;
+}
